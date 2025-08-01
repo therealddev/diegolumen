322091f2544bca9c4cd759e5a02390810a808fa2
Diego <dserrani99@gmail.com>
2025-07-31 22:42:39 -0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Outlet />
+      
+    </div>
+  );
+}
+
+export default App;
